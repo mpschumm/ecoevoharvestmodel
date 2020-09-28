c53cdd97f0c9152080393e99b375c264fbe94ffc
Matthew Schumm <mschumm@uchicago.edu>
2020-09-28 19:16:17 -0400

    Changed terminology
    mainly to refer to energy intake I as p

--- growth_functions.R
+++ growth_functions.R
@@ -9,8 +9,8 @@ intrinsic_lambda <- function(l, l_bar, r, lambda_min, lambda_max) {
 }
 
 # Energy intake for the fish per year, dependent on fish structural biomass
-size_dependent_energy_intake <- function(S, I_0, I_1) {
-  return(I_0*(S)^(I_1))
+size_dependent_energy_intake <- function(S, p_0, p_1) {
+  return(resource*p_0*(S)^(p_1))
 }
 
 # Calculate the cost of the year of maintaining current structural and reversible biomass
@@ -19,13 +19,13 @@ maintenance_cost <- function(S, E, c_S, c_E) {
 }
 
 # Function for calculating a new, reduced level of reversible biomass, if net energy intake is negative
-negative_I_net <- function(E, I_net, e_E) {
-  if (I_net < 0) {
-    return(E + (I_net/e_E))}
+negative_p_net <- function(E, p_net, e_E) {
+  if (p_net < 0) {
+    return(E + (p_net/e_E))}
 }
 
 # Calculate the amount of energy needed to raise the R/S ratio to the length-dependent maximum
-I_E_raising_lambda <- function(lambda_a, S, E) {
+p_E_raising_lambda <- function(lambda_a, S, E) {
   return((lambda_a*S - E))
 }
 
@@ -38,25 +38,25 @@ remaining_energy_allocation <- function(e_S, e_E, lambda_a) {
 get_pre_reproductive_size <- function(E_past, l_past, a) {
   S_past = structural_mass(l_past)
   lambda_a = intrinsic_lambda(l_past, l_bar, r, lambda_min, lambda_max)
-  I_net = size_dependent_energy_intake(S_past, I_0, I_1) - maintenance_cost(S_past, E_past, c_S, c_E)
-  if (I_net < 0) {
-    E <- max(0,negative_I_net(E, I_net, e_E))
+  p_net = size_dependent_energy_intake(S_past, p_0, p_1) - maintenance_cost(S_past, E_past, c_S, c_E)
+  if (p_net < 0) {
+    E <- max(0,negative_p_net(E, p_net, e_E))
   }
-  if (I_net >= 0 & lambda_a > (E_past/S_past)) {
+  if (p_net >= 0 & lambda_a > (E_past/S_past)) {
     # Gives amount of grams needed to raise E/S to lambda_l - not taking efficiency into account yet
-    I_E = I_E_raising_lambda(lambda_a, S_past, E_past)
-    if ((I_E/e_E - I_net) >=0) {
-      E_past = I_net*e_E + E_past
-      I_net = 0
+    p_E = p_E_raising_lambda(lambda_a, S_past, E_past)
+    if ((p_E/e_E - p_net) >=0) {
+      E_past = p_net*e_E + E_past
+      p_net = 0
     }
-    if ((I_E/e_E - I_net) <0) {
-      E_past = I_E + E_past
-      I_net = I_net - I_E/e_E
+    if ((p_E/e_E - p_net) <0) {
+      E_past = p_E + E_past
+      p_net = p_net - p_E/e_E
     }
   }
-  if (I_net >= 0) {
-    E = E_past + remaining_energy_allocation(e_S, e_E, lambda_a)*e_E*I_net
-    S = S_past + (1-remaining_energy_allocation(e_S, e_E, lambda_a))*e_S*I_net
+  if (p_net >= 0) {
+    E = E_past + remaining_energy_allocation(e_S, e_E, lambda_a)*e_E*p_net
+    S = S_past + (1-remaining_energy_allocation(e_S, e_E, lambda_a))*e_S*p_net
   }
   l = (S/c_1)^(1/3)
   output <- c(E,l)
