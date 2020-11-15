3d64c0888bb8ddf1f5fff41e41fb0997e446787d
Matthew Schumm <mschumm@uchicago.edu>
2020-11-15 07:59:09 -0500

    Smithsonian file, selection differential, misc

---
+++ Smithsonian_Fig2.R
@@ -0,0 +1,3 @@
+M_saxitilis<-Chesapeake_fish_collections[which(Chesapeake_fish_collections$M_saxitilis>0),c(1,2)]
+P_dentatus<-Chesapeake_fish_collections[which(Chesapeake_fish_collections$P_dentatus>0),c(1,3)]
+P_saltatrix<-Chesapeake_fish_collections[which(Chesapeake_fish_collections$P_saltatrix>0),c(1,4)]
