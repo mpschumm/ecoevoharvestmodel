f14a0c4ed91a007e14a8d2ec05d477598acfedb2
Matthew Schumm <mschumm@uchicago.edu>
2022-05-29 14:38:15 -0400

    Function for extracting model run output
    Function for extracting model run output

---
+++ extracting_output.R
@@ -0,0 +1,11 @@
+extract_output <- function(object) {
+data_list <- set_1 # Use whatever the name is of the workspace object to which the results of the mclapply run were assigned 
+no_runs <-7000 # How many runs are contained in the object to which the mclapply results were assigned?
+mat = matrix(, nrow = no_runs, ncol = 16) # Set up a matrix to hold the results
+mapply(function(x) mapply(function(y) try(mat[x,y] <<- data_list[[x]][[y]]), y=c(1:6,12:13)), x=c(1:no_runs))
+output_all <- as.data.frame(mat)
+output_all <- output_all[,c(1:6,12:13)]
+output_all <- na.omit(output_all)
+colnames(output_all) <- c("phys", "AAM", "LAM", "L2", "L3", "Linf", "K_var", "F")
+return(output_all)
+}
