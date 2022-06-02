58ea6166e8aa5aad4c78af1fc320002fa196e4b1
Matthew Schumm <mschumm@uchicago.edu>
2022-06-01 22:03:00 -0400

    Adding argument for number of runs done
    Adding argument for number of runs done

--- extracting_output.R
+++ extracting_output.R
@@ -1,6 +1,6 @@
-extract_output <- function(object) {
+extract_output <- function(object, no_runs) {
 data_list <- object # Use whatever the name is of the workspace object to which the results of the mclapply run were assigned 
-no_runs <-7000 # How many runs are contained in the object to which the mclapply results were assigned?
+no_runs <-no_runs # How many runs are contained in the object to which the mclapply results were assigned?
 mat = matrix(, nrow = no_runs, ncol = 16) # Set up a matrix to hold the results
 mapply(function(x) mapply(function(y) try(mat[x,y] <<- data_list[[x]][[y]]), y=c(1:6,12:13)), x=c(1:no_runs))
 output_all <- as.data.frame(mat)
